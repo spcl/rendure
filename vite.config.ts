56434115ec5b1643cf03c289fd9dae5a333b6fb8
Philipp Schaad <schaad.phil@gmail.com>
2025-07-16 15:42:55 +0200

    Emit types

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+    plugins: [
+        dts({
+            tsconfigPath: resolve(__dirname, './tsconfig-build.json'),
+        }),
+    ],
     root: 'src',
     build: {
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
             formats: ['es'],
+            name: 'rendure',
+            fileName: 'rendure',
         },
-        outDir: '../dist',
+        outDir: resolve(__dirname, 'dist'),
         emptyOutDir: true,
         sourcemap: true,
     },
@@ -18,7 +26,7 @@ export default defineConfig({
     },
     resolve: {
         alias: {
-            '@': '/src',
+            '@': resolve(__dirname, 'src'),
         },
     },
 });
