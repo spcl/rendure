7b7a458d260c9d54fadc0a8ecf838f47caf7a670
Philipp Schaad <schaad.phil@gmail.com>
2025-07-24 15:07:35 +0200

    Add missing export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 
 export * from './renderer/core/common/renderable';
 export * from './renderer/core/common/renderer_base';
+export * from './renderer/core/common/renderer_ui';
 export * from './renderer/core/common/renderer_utils';
 export * from './renderer/core/html_canvas/canvas_manager';
 export * from './renderer/core/html_canvas/html_canvas_renderable';
